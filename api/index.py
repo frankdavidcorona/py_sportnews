faaa4dabe0ab5f281fb281f347b3072957846696
Frank Corona Prendes <frank.corona@pm.me>
2023-04-15 17:38:17 -0300

    feat(api): add news scraping endpoint This commit adds a new endpoint to the API that scrapes news headlines and images from the AS website. The endpoint is located at `/api/news` and returns a JSON object containing an array of news headlines and their corresponding image URLs. The `sanitize_text` function is used to clean up the text scraped from the website. The `scrape_news_headlines` function is used to scrape the headlines and images from the website. The `get_news` function is used to handle the GET request to the `/api/news` endpoint and returns the scraped headlines and images as a JSON object.

---
+++ api/index.py
@@ -0,0 +1,65 @@
+import requests
+import re
+from bs4 import BeautifulSoup
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+
+def sanitize_text(text):
+    # Remove HTML entities
+    text = re.sub(r'&[^;]+;', '', text)
+
+    # Replace multiple spaces or line breaks with a single space
+    text = re.sub(r'\s+', ' ', text)
+
+    # Remove all non-ASCII Unicode characters
+    text = re.sub(r'[^\x00-\x7F]+', '', text)
+
+    # Strip leading and trailing spaces
+    text = text.strip()
+
+    return text
+
+
+def scrape_news_headlines():
+    url = 'https://www.as.com'
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, 'html.parser')
+
+    headlines_and_images = []
+
+    # Find all 'article' elements
+    articles = soup.find_all('article')
+
+    # Loop through each 'article' element
+    for article in articles:
+        # Find the 'h2' element within the 'article'
+        h2 = article.find('h2')
+        if h2 is not None:
+            headline = sanitize_text(h2.get_text())
+        else:
+            continue
+
+        # Find the 'figure' element within the 'article' and extract the image URL
+        figure = article.find('figure')
+        img_url = None
+        if figure is not None:
+            img = figure.find('img')
+            if img is not None:
+                img_url = img.get('src')
+
+        headlines_and_images.append(
+            {'headline': headline, 'image_url': img_url})
+
+    return headlines_and_images
+
+
+@app.route('/api/news', methods=['GET'])
+def get_news():
+    headlines = scrape_news_headlines()
+    return jsonify(headlines)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
